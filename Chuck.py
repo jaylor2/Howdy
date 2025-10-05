cec001c402d0c2499ae6c7c2bce22a5767570bce
Jaylor <michael.jaylor.wilson@gmail.com>
2025-10-05 14:02:32 -0700

    updated Chuck.py with comments

--- Chuck.py
+++ Chuck.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+#'import' brings 'requests' library into scope.
 import requests
+
+#'r' is the name of the variable where the result of the GET is stored.
+#'=' is an assignment operator, tells Python to store GET result in 'r'
 r = requests.get('https://api.chucknorris.io/jokes/random')
+
+#'data' is the variable where JSON().GET request will be stored.
+#'r.son().get('value')'  tells Python to get the 'value' from the JSON data
 data = r.json().get('value')
+
+#print(data) tells Python to print the result of 'data'
 print(data)
 
